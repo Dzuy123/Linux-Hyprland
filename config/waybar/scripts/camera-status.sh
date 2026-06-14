384d42aa0d217f7a9fc9c2b43e559f9e5880f5d7
Dzuy123 <kayne6842@gmail.com>
2026-06-14 10:08:49 +0700

    feat: camera indicator in waybar center with blink animation

--- config/waybar/scripts/camera-status.sh
+++ config/waybar/scripts/camera-status.sh
@@ -2,5 +2,5 @@
 if fuser /dev/video0 /dev/video1 /dev/video2 2>/dev/null | grep -q .; then
     echo '{"text": "󰄀", "tooltip": "Camera in use", "class": "active"}'
 else
-    echo ''
+    echo '{"text": "󰄀", "tooltip": "Camera off", "class": ""}'
 fi
